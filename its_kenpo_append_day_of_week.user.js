d9cebe37a16cba39e88d129ed06299f8d0e7b2e1
kurikei <kurikei@gmail.com>
2018-05-09 20:10:52 +0900

    ITS健保の施設予約システムのフォームの日付に曜日を付与するユーザースクリプトを追加した

---
+++ its_kenpo_append_day_of_week.user.js
@@ -0,0 +1,40 @@
+// ==UserScript==
+// @name         ITS施設予約システム - 日付に曜日を付与する
+// @namespace    https://github.com/kurikei
+// @version      1.0.0
+// @description  ITS施設予約システムの日付に曜日を付与する
+// @author       kurikei
+// @require      https://cdnjs.cloudflare.com/ajax/libs/babel-standalone/6.18.2/babel.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/babel-polyfill/6.16.0/polyfill.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.20.1/moment.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.20.1/locale/ja.js
+// @match        https://as.its-kenpo.or.jp/apply/new?*
+// @match        https://as.its-kenpo.or.jp/apply/empty_new?*
+// ==/UserScript==
+
+/* jshint ignore:start */
+var inline_src = (<><![CDATA[
+  /* jshint ignore:end */
+  /* jshint esnext: false */
+  /* jshint esversion: 6 */
+  moment.locale('ja');
+
+  const options = document.getElementById('apply_join_time').getElementsByTagName('option');
+
+  for (const option of options) {
+    if (option.innerText === '') {
+      continue;
+    }
+    option.innerText = append_day_of_week(option.innerText);
+  }
+
+  function append_day_of_week(dateString) {
+    const date = moment(dateString, 'YYYY年MM月DD日');
+    return `${dateString}(${date.format('ddd')})`;
+  }
+
+  /* jshint ignore:start */
+]]></>).toString();
+var c = Babel.transform(inline_src, { presets: [ "es2015", "es2016" ] });
+eval(c.code);
+/* jshint ignore:end */
