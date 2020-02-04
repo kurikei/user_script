30af6cc77f019f6901af75eb2fc586437a653ef0
kurikei <kurikei@gmail.com>
2020-02-04 13:00:20 +0900

    バージョンをアップデートする

---
+++ bin/replace_version.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+set -eux
+current_version=$(cat $1 | perl -lne 'print $1 if /\@version\s+([0-9\.]+)/')
+# XXX: プロジェクトルートから立たれる前提
+new_version=$(./bin/increment_semantic_version.sh -m $current_version)
+perl -i -pne "s/${current_version}/${new_version}/" $1
