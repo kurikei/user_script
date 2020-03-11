1ca24e20cb8075fede8501b2d00ec14dc2d79450
kurikei <kurikei@gmail.com>
2020-03-11 12:19:46 +0900

    日経xtechの記事を印刷表示ページにリダイレクトする

---
+++ nikkei_xtech_print_viewer.user.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name         日経xtechを印刷表示にする
+// @namespace    http://github.com/kurikei
+// @version      1.0.0
+// @author       kurikei
+// @require      https://cdnjs.cloudflare.com/ajax/libs/babel-standalone/6.18.2/babel.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/babel-polyfill/6.16.0/polyfill.js
+// @match       https://xtech.nikkei.com/atcl/nxt/column/*
+// ==/UserScript==
+
+/* jshint ignore:start */
+var inline_src = (<><![CDATA[
+/* jshint ignore:end */
+/* jshint esnext: false */
+/* jshint esversion: 6 */
+
+  const url = new URL(location.href);
+
+  if (url.searchParams.has("ST")) {
+    // do nothing
+  } else {
+    url.searchParams.set('ST', 'print')
+    location.href = url.href;
+  }
+
+/* jshint ignore:start */
+]]></>).toString();
+var c = Babel.transform(inline_src, { presets: [ "es2015", "es2016" ] });
+eval(c.code);
+/* jshint ignore:end */
