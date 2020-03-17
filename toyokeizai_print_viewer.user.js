3372cda327a2629b5790dd53db077e0645a3bfe7
circleci <kurikei+circleci@gmail.com1>
2020-03-17 07:24:17 +0000

    increment version

--- toyokeizai_print_viewer.user.js
+++ toyokeizai_print_viewer.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         東洋経済ONLINE/wedgeの記事を印刷表示にする
 // @namespace    http://github.com/kurikei
-// @version      1.2.0
+// @version      1.3.0
 // @author       kurikei
 // @match        https://toyokeizai.net/articles/-/*
 // @match        https://wedge.ismedia.jp/articles/-/*
